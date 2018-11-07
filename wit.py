1bc2845704790774e482e3e014482cd68af9f8a1
Fede Pujol <federicodpujol@gmail.com>
2018-11-07 17:50:44 -0300

    Impresion de color por pantalla de temperatura. (Broken)

--- wit.py
+++ wit.py
@@ -1,11 +1,17 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 import json
 import click
 import requests
 
 def get_json(queryParams):
-    url = 'http://api.openweathermap.org/data/2.5/weather'
+    url = 'http://api.openweathermap.org/data/2.5/'
+
+    if queryParams['weather']:
+        url = url + 'weather'
+    else:
+        url = url + 'forecast'
+    
     res = requests.get(url, params=queryParams)
 
     dt = res.json()
@@ -21,6 +27,13 @@ def get_json(queryParams):
 
     return data
 
+def display_data(data):    
+    click.secho('\t' + data['name'])
+    click.secho('\tOpenWeather ID: {}'.format(data['id']))
+    click.secho('\tTemperatura Actual: {}'.format(data['temp_actual']) + '°')
+    click.secho('\tTemperatura Maxima: {}'.format(data['temp_max']) + '°', fg='red', bold=True)
+    click.secho('\tTemperatura Minima: {}'.format(data['temp_min']) + '°', fg='cyan', bold=True)    
+    
 @click.command()
 @click.option('--city', '-c', default="", help='Name of the City')
 @click.option('--id', '-i', default="", help='OpenWeather ID of the City ')
@@ -28,24 +41,33 @@ def get_json(queryParams):
 @click.option('--lang', '-l', default="es", help='Set request lenguage')
 @click.option('--units', '-u', default='metric', help='Unit format. The default format is metric',
 type=click.Choice(['metric', 'imperial']))
-def main(city, id, api_key, lang, units):
-	
+@click.option('--weather', 'time', default=True,  help='Get current weather. Default option')
+@click.option('--forecast', 'time', default=False, help='Get forecast of the city. Current daily')
+def main(city, id, api_key, lang, units, time):
 
-	if api_key is not "":
-		apikey = api_key
+    if api_key is not "":
+        apikey = api_key
 
-	query_params = {
+    query_params = {
 		'appid': apikey,
 		'lang': lang,
-        'units': units
-	}	
-	
-	if city is "":
-		query_params['id'] = id
-	else:
-		query_params['q'] = city
+        'units': units,
+        'weather': 'false'
+        'forecast': 'false'
+    }
 
-	print(get_json(query_params))
+    if time:
+        query_params['weather'] = 'true'
+    else:
+        query_params['forecast'] = 'true'
+   
+    if city is "":
+        query_params['id'] = id
+    else:
+        query_params['q'] = city
+        
+    data = get_json(query_params)
+    display_data(data)
 
 if __name__ == "__main__":
 	main()
